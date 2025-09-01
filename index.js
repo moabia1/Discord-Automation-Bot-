745eff7ca867074a2bd1c9c7b44c63c9c080ac64
moabia1 <mdmoabia81@gmail.com>
2025-09-01 23:22:25 +0530

    set-up normal reply from bot function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const { Client, GatewayIntentBits } = require("discord.js")
 
 const client = new Client({
@@ -7,3 +8,14 @@ const client = new Client({
     GatewayIntentBits.MessageContent
   ]
 })
+
+client.once("ready", () => {
+  console.log("Bot is Ready")
+})
+
+client.on("messageCreate", (message) => { 
+  console.log("message by you-",message)
+  message.reply("Hello! This response from bot")
+})
+
+client.login(process.env.DISCORD_BOT_TOKEN)
