43ff1c0c38bdaa9a5f188bd3a9d3f670355406dd
moabia1 <mdmoabia81@gmail.com>
2025-09-02 10:10:33 +0530

    complete text generation automation

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 require("dotenv").config()
-const { Client, GatewayIntentBits } = require("discord.js")
+const { Client, GatewayIntentBits, AttachmentBuilder } = require("discord.js")
+const {GoogleGenAI} = require("@google/genai")
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+
+async function generateContent(prompt) {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: prompt,
+    config: {
+      systemInstruction: `
+        "always take answer short never take answer upto 3500 words and formatting in good structure."
+      `
+    }
+  });
+
+  return response.text
+}
 
 const client = new Client({
   intents: [
@@ -13,11 +29,15 @@ client.once("ready", () => {
   console.log("Bot is Ready")
 })
 
-client.on("messageCreate", (message) => { 
+client.on("messageCreate", async (message) => {
+
   const isBot = message.author.bot;
   if (isBot) return
-  
-  message.reply("Hello From Bot")
-})
 
+  const content = await generateContent(message.content)
+  if (content) {
+    message.reply(content)
+  }
+})
+  
 client.login(process.env.DISCORD_BOT_TOKEN)
